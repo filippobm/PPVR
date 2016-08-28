136f1f6743596fca2338b3c7d70f4ee3b7cb9d5c
Filippo <bretas.filippo@gmail.com>
2016-08-27 22:12:08 -0300

    Cria pasta para as ViewModels. Separa as ViewModels do arquivo AccountViewModel.cs e coloca na pasta ViewModels\Account

--- PPVR.WebApp/Controllers/AccountController.cs
+++ PPVR.WebApp/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using PPVR.WebApp.Models;
+using PPVR.WebApp.ViewModels.Account;
 
 namespace PPVR.WebApp.Controllers
 {
---
+++ PPVR.WebApp/ViewModels/Account/ExternalLoginListViewModel.cs
@@ -0,0 +1,7 @@
+namespace PPVR.WebApp.ViewModels.Account
+{
+    public class ExternalLoginListViewModel
+    {
+        public string ReturnUrl { get; set; }
+    }
+}

3d52891e891611ede9793a965671bc18b8a775e6
Filippo <bretas.filippo@gmail.com>
2016-08-28 12:49:39 -0300

    Adiciona projeto que sera utilizado para fazer o Benchmark das libs de OCR.

---
+++ PPVR.OCRBenchmark/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace PPVR.OCRBenchmark
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var t = MainAsync(args);
+            t.Wait();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+        }
+    }
+}
---
+++ PPVR.OCRBenchmark/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PPVR.OCRBenchmark")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PPVR.OCRBenchmark")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a72ed2e8-80d5-4977-8bd0-dea13d4184e1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

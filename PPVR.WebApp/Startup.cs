0315cd32dfce1865be9fa4fd1b04551fad9841c3
ulata <ulata@DESKTOP-IFARB5K>
2016-08-27 21:50:34 -0300

    Add project files.

---
+++ PPVR.WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PPVR.WebApp.Startup))]
+namespace PPVR.WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

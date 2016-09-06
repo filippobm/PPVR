b509681348fcc30805b09134304ac4715e8548af
Filippo <bretas.filippo@gmail.com>
2016-09-05 22:30:41 -0300

    clean up

--- PPVR.DataImport/Program.cs
+++ PPVR.DataImport/Program.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PPVR.DataImport
+﻿namespace PPVR.DataImport
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
         }
     }

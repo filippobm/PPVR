6f8c03853ba1229bdf71d2676181e7f6e13fb082
Filippo <bretas.filippo@gmail.com>
2016-10-30 14:53:47 -0200

    Upload pictures

--- PPVR.WebApp/Controllers/HomeController.cs
+++ PPVR.WebApp/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
 
 namespace PPVR.WebApp.Controllers
 {
@@ -13,6 +16,28 @@ namespace PPVR.WebApp.Controllers
         public ActionResult Sobre()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadPicture(HttpPostedFileBase file)
+        {
+            if (file == null || file.ContentLength <= 0)
+                return RedirectToAction("Index");
+
+            var extension = Path.GetExtension(file.FileName);
+
+            if (extension != null &&
+                (extension.ToLower().EndsWith(".png") || extension.ToLower().EndsWith(".jpg") ||
+                 extension.ToLower().EndsWith(".jpge")))
+            {
+                var path = Server.MapPath($"~/Content/images/santinhos/{User.Identity.Name}/");
+                Directory.CreateDirectory(path);
+                var filePath = $"{path}{Guid.NewGuid()}.{extension}";
+                ViewBag.FilePath = filePath;
+                file.SaveAs(filePath);
+                return RedirectToAction("Index");
+            }
+            throw new Exception("AAAAA");
         }
     }
 }

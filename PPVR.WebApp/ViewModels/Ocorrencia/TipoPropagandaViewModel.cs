779d14ed8f6214c543624e4b6f454bcd8f19e19b
Filippo <bretas.filippo@gmail.com>
2016-11-18 16:26:44 -0200

    remove tipopropagandaviewmodel.cs

--- PPVR.WebApp/ViewModels/Ocorrencia/TipoPropagandaViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PPVR.WebApp.ViewModels.Ocorrencia
-{
-    public class TipoPropagandaViewModel
-    {
-        public int TipoPropagandaId { get; set; }
-        public string Descricao { get; set; }
-    }
-}

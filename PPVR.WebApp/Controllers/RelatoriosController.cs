10358fbc21624cff1bdc3b02f50a4ea249d7b906
Filippo <bretas.filippo@gmail.com>
2016-11-22 03:49:34 -0200

    cria relatórios.

---
+++ PPVR.WebApp/Controllers/RelatoriosController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace PPVR.WebApp.Controllers
+{
+    public class RelatoriosController : Controller
+    {
+        // GET: Relatorios
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
